2b3728458af2b26f44070ef2d7ee7270dda442f0
Salaheddinebacha <Salaheddinebacha03@gmail.com>
2024-07-06 14:23:14 +0100

    Delete rootReducer.js

--- rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import navigationReducer from "./navigationReducer";
-
-const rootReducer = combineReducers({
-  navigation: navigationReducer,
-});
-
-export default rootReducer;
